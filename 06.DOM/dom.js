4c1f68cddf69d52c6697d729996fb314b3592fce
aleksandar <a.petkovpetkov@gmail.com>
2019-11-19 17:28:11 +0200

    Add exercises including DOM manipulations

--- 06.DOM/dom.js
+++ 06.DOM/dom.js
@@ -1,4 +1,4 @@
 //          Document Object Model
 //We traverse the DOM tree. All HTML documents are store as DOM trree
-//XSS  Attacks
-//BOM Browser Object Model
+//we can get elements (byId, byClass and so on)
+// Live vs Static Selector Exp: getElementsByTagName() vs querySelectorAll();
