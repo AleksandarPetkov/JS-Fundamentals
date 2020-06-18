f2cade420498e6dac7208a6b9179419b5aaf9a18
aleksandar <a.petkovpetkov@gmail.com>
2020-06-18 15:17:45 +0300

    Resolve Growing colors issue

--- 00_DOM/03_Growing-Word/solution.js
+++ 00_DOM/03_Growing-Word/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-   //TODO 
+   let index = 0;
+   let currentPx = 1;
+   document.getElementById("exercise").addEventListener("click", function(){
+     let arrayColors = ["blue", "green", "red"];
+
+     if(index == 0){
+      document.getElementById("exercise").childNodes[5].style.color = arrayColors[index];
+      currentPx *= 2;
+      document.getElementById("exercise").childNodes[5].style.fontSize = currentPx.toString() + "px";
+      index++;
+     } else if (index == 1){
+      document.getElementById("exercise").childNodes[5].style.color = arrayColors[index];
+      currentPx *= 2;
+      document.getElementById("exercise").childNodes[5].style.fontSize = currentPx.toString() + "px";
+      index++;
+     } else if (index == 2){
+      document.getElementById("exercise").childNodes[5].style.color = arrayColors[index];
+      currentPx *= 2;
+      document.getElementById("exercise").childNodes[5].style.fontSize = currentPx.toString() + "px";
+      index = 0;
+     }
+   });
+
 }
