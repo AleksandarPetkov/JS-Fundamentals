2ee6c705e2f934ac112efc48b213fa01703afc0c
aleksandar <a.petkovpetkov@gmail.com>
2020-06-18 11:26:50 +0300

    Resolve Format the text issue

--- 00_DOM/02_Format-the-Text/solution.js
+++ 00_DOM/02_Format-the-Text/solution.js
@@ -1,4 +1,33 @@
 function solve(){
-  //TODO
-  
+  let wholeText = document.getElementById('input').textContent;
+  let splittedText = wholeText.split('.');
+  let countOfsentences = splittedText.length - 2;
+
+  let count = 0;
+  let currentSentences = '';
+  for (i = 0; i <= countOfsentences; i++) {
+    
+    currentSentences += splittedText[i] + '.';
+    count++;
+
+    if(count == 3){
+      let newParagraf = document.createElement('p');
+
+      var text = document.createTextNode(currentSentences);
+      newParagraf.appendChild(text);
+
+      document.getElementById('output').appendChild(newParagraf);
+      currentSentences = '';
+      count = 0;
+    }
+  }
+
+  if(count == 1 || count == 2){
+    let newParagraf = document.createElement('p');
+
+    var text = document.createTextNode(currentSentences);
+    newParagraf.appendChild(text);
+
+    document.getElementById('output').appendChild(newParagraf);
+  }
 }
